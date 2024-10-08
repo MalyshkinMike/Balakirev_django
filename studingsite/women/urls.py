7accc074716b7b7b464be9efc76c27d6d36a5631
Mike <mikhail.malishkin@gmail.com>
2024-10-08 22:49:09 +0400

    1.6 Динамические URL. Пользовательские конвертеры

--- studingsite/women/urls.py
+++ studingsite/women/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from . import views
+from django.urls import path, re_path, register_converter
+from . import views, converters
+
+
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index), # http://127.0.0.1:8000/
-    path('cats/', views.categories) # http://127.0.0.1:8000/cats
+    path('cats/<int:cat_id>/', views.categories), # http://127.0.0.1:8000/cats
+    path('cats/<slug:cat_slug>/', views.categories_by_slug), # http://127.0.0.1:8000/cats
+    path('archive/<year4:year>/', views.archive)
 ]
