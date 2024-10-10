fdc7aecdd1b7d3aa9a4dba2f4cb3b1a438088f15
Mike <mikhail.malishkin@gmail.com>
2024-10-10 22:42:50 +0400

    1.8 Перенаправления (redirect). Функция reverse

--- studingsite/women/views.py
+++ studingsite/women/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 # Create your views here.
 def index(request): # HTTPRequest
@@ -15,7 +17,8 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        raise Http404()
+        uri = reverse('cats', args=('sport', ))
+        return HttpResponsePermanentRedirect(uri) # redirect(uri)
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
 
 def page_not_found(request, exception):
