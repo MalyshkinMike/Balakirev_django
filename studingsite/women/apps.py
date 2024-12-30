aca909fb0b478f9b5dff4f6f8790502941fb9d4e
Mike <mikhail.malishkin@gmail.com>
2024-12-30 14:38:08 +0400

    6.1 Подключение админ-панели. Регистрация моделей

--- studingsite/women/apps.py
+++ studingsite/women/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class WomenConfig(AppConfig):
+    verbose_name = 'Женщины мира'
+    verbose_name_plural = 'Женщины мира'
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'women'
