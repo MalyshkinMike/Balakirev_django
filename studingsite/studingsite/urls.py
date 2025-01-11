d885f0b3aff17231d501b2d3a07c518078f61fe6
Mike <mikhail.malishkin@gmail.com>
2025-01-11 15:02:12 +0400

    7.8 Отображение изображений в HTML-документе и админ-панели

--- studingsite/studingsite/urls.py
+++ studingsite/studingsite/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from women.views import page_not_found
@@ -26,6 +27,8 @@ urlpatterns = [
     path('', include('women.urls')),
 ] + debug_toolbar_urls()
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
 
