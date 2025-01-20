17ef2af39a62c17f531c4969865f51da3c1afc6f
Mike <mikhail.malishkin@gmail.com>
2025-01-20 22:37:50 +0400

    9.10 Восстановление пароля. Идея алгоритма

--- studingsite/studingsite/settings.py
+++ studingsite/studingsite/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 from django.conf.global_settings import STATICFILES_DIRS, MEDIA_ROOT, MEDIA_URL, LOGIN_REDIRECT_URL, \
-    LOGOUT_REDIRECT_URL, AUTHENTICATION_BACKENDS
+    LOGOUT_REDIRECT_URL, AUTHENTICATION_BACKENDS, EMAIL_BACKEND
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,3 +149,5 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'users.authentication.EmailAuthBackend'
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
