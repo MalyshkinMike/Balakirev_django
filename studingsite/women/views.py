ce91735ce6be45615a6064f11de3758891fe00eb
Mike <mikhail.malishkin@gmail.com>
2025-01-20 10:55:59 +0400

    9.5 Декоратор login_required и класс LoginRequiredMixin

--- studingsite/women/views.py
+++ studingsite/women/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect, get_object_or_404
@@ -27,7 +29,7 @@ class WomenHome(DataMixin, ListView):
         return Women.published.all().select_related('cat')
 
 
-
+@login_required
 def about(request):
     contact_list = Women.published.all()
     paginator = Paginator(contact_list, 3)
@@ -42,10 +44,16 @@ def about(request):
                    })
 
 
-class AddPage(DataMixin, CreateView):
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
     title_page = 'Добавление статьи'
+    # login_url = '/admin/'
+
+    def form_valid(self, form):
+        w = form.save(commit=False)
+        w.author = self.request.user
+        return super().form_valid(form)
 
 
 
